1b9fbcd5893c2cbd6493e090c6080235c88ebb4e
mohsak13 <mohammedshakokah@gmail.com>
2024-10-08 10:31:33 +0300

    update store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import { thunk } from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
+// import { composeWithDevTools } from "redux-devtools-extension";
 
 import {
   createPostReducer,
@@ -51,7 +51,8 @@ const middleware = [thunk];
 
 const store = createStore(
   reducer,
-  composeWithDevTools(applyMiddleware(...middleware))
+  // composeWithDevTools(applyMiddleware(...middleware))
+  applyMiddleware(...middleware)
 );
 
 export default store;
